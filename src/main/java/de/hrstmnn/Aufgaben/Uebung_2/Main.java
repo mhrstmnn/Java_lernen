7e3f652f7da63305d5d3b78f80edb1d931ccdc47
Michael Horstmann <git@hrstmnn.de>
2023-02-09 15:18:17 +0100

    removed unused main classes

--- src/main/java/de/hrstmnn/Aufgaben/Uebung_2/Main.java
+++
@@ -1,7 +0,0 @@
-package de.hrstmnn.Aufgaben.Uebung_2;
-
-public class Main {
-    public static void hello(String[] args) {
-        System.out.println("Hallo Welt");
-    }
-}
--- src/main/java/de/hrstmnn/Main.java
+++
@@ -1,7 +0,0 @@
-package de.hrstmnn;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
